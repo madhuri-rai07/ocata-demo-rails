3cd783d802ba509df967353659235ae21e619806
OTSUKA, Yuanying <yuanying@fraction.jp>
2016-10-05 08:34:07 +0900

    Resize using memory

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -7,8 +7,6 @@ class PostTest < ActiveSupport::TestCase
 
   test "the memory use" do
     a = Array.new(90000000, Array.new(9000000, Array.new(9000000, 'MemoryXXX')))
-    a << a.dup
-    a << a.dup
     sleep 2400
     assert a
   end
