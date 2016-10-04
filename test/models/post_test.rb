84bcc90c4a43d3276d2f89a488f7237cf11a8acc
OTSUKA, Yuanying <yuanying@fraction.jp>
2016-10-04 14:18:14 +0900

    Add memory use test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "the memory use" do
+    a = Array.new(9000000, Array.new(9000000, Array.new(900000, 'Memory')))
+    sleep 300
+    assert a
+  end
 end
