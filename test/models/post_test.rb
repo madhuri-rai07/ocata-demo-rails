fdc62273a9a5514a7e455c591221da8e459cf9d6
OTSUKA, Yuanying <yuanying@fraction.jp>
2016-10-04 17:57:38 +0900

    Increase memory usage

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -6,7 +6,7 @@ class PostTest < ActiveSupport::TestCase
   end
 
   test "the memory use" do
-    a = Array.new(9000000, Array.new(9000000, Array.new(9000000, 'MemoryXXX')))
+    a = Array.new(90000000, Array.new(9000000, Array.new(9000000, 'MemoryXXX')))
     a << a.dup
     a << a.dup
     sleep 2400
